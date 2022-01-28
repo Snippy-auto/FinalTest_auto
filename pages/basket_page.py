8cdd799053e346c34e4ef86482142b14cecbfda1
tlkosha <95431054+tlkosha@users.noreply.github.com>
2022-01-29 01:57:52 +0300

    Create basket_page.py

---
+++ pages/basket_page.py
@@ -0,0 +1,11 @@
+from .locators import BasketPageLocators
+from .base_page import BasePage
+
+class BasketPage(BasePage):
+
+    def should_be_no_goods_in_basket(self):
+        assert self.is_not_element_present(*BasketPageLocators.GOODS_IN_BASKET)
+    
+    def should_be_basket_is_empty(self):
+    	assert self.browser.find_element(*BasketPageLocators.YOUR_BASKET_EMPTY).text == "Your basket is empty. " \
+                                                                                          "Continue shopping"
