c5adabcb252673f07c23ada2fa3b2e7926d1d347
tlkosha <95431054+tlkosha@users.noreply.github.com>
2022-01-29 01:42:25 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+#функция для того, чтобы можно было выставить язык в консоли для страницы
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default='en',
                      help="Choose browser: chrome or firefox")
@@ -12,7 +13,7 @@ def browser(request):
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': language})
     browser = webdriver.Chrome(options=options)
-
+#
     yield browser
     print("\nquit browser..")
     browser.quit()
