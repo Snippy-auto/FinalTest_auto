d973f485dea5471ade6ee927a4f26ea1cd013173
tlkosha <95431054+tlkosha@users.noreply.github.com>
2022-01-29 01:43:43 +0300

    Update test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,51 @@
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+import time
+import pytest
+from .pages.basket_page import BasketPage
+from .pages.product_page import ProductPage
+
+
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self,browser):
+        link = "http://selenium1py.pythonanywhere.com"
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()          # выполняем метод страницы — переходим на страницу логина
+
+
+    def test_guest_should_see_login_link(self,browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
+
+def test_correct_url_address(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    login_page = LoginPage(browser, link)
+    login_page.open()
+    login_page.should_be_login_url()
+
+def test_see_form_auth(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    login_page = LoginPage(browser, link)
+    login_page.open()
+    login_page.should_be_login_form()
+
+def test_see_form_reg(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    login_page = LoginPage(browser, link)
+    login_page.open()
+    login_page.should_be_register_form()
+
+    
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_go_to_basket()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_basket_is_empty()
+
