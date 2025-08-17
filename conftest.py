3cc3eef41bfa3b46b1ea905b8fe71bef4274bb33
Snippy-auto <snippy.xperia@gmail.com>
2025-08-17 19:28:25 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,19 +1,19 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 
-#функция для того, чтобы можно было выставить язык в консоли для страницы
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default='en',
-                     help="Choose browser: chrome or firefox")
+                     help="Choose language: e.g. en, ru")
 
 @pytest.fixture(scope="function")
 def browser(request):
     language = request.config.getoption("language")
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': language})
-    browser = webdriver.Chrome(options=options)
-#
+    browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
     yield browser
     print("\nquit browser..")
     browser.quit()
