c4fc5cf430f185c37b2643ff14a6691cf4e09d44
Snippy-auto <snippy.xperia@gmail.com>
2025-08-17 19:30:22 +0300

    Update test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,22 +1,19 @@
-from .pages.main_page import MainPage
-from .pages.login_page import LoginPage
-import time
 import pytest
-from .pages.basket_page import BasketPage
-from .pages.product_page import ProductPage
-
+from pages.main_page import MainPage
+from pages.login_page import LoginPage
+from pages.basket_page import BasketPage
+from pages.product_page import ProductPage
 
 class TestLoginFromMainPage():
-    def test_guest_can_go_to_login_page(self,browser):
+    def test_guest_can_go_to_login_page(self, browser):
         link = "http://selenium1py.pythonanywhere.com"
         page = MainPage(browser, link)
         page.open()
         page.go_to_login_page()
         login_page = LoginPage(browser, browser.current_url)
-        login_page.should_be_login_page()          # выполняем метод страницы — переходим на страницу логина
+        login_page.should_be_login_page()
 
-
-    def test_guest_should_see_login_link(self,browser):
+    def test_guest_should_see_login_link(self, browser):
         link = "http://selenium1py.pythonanywhere.com/"
         page = MainPage(browser, link)
         page.open()
@@ -28,11 +25,13 @@ def test_correct_url_address(browser):
     login_page.open()
     login_page.should_be_login_url()
 
+
 def test_see_form_auth(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
     login_page = LoginPage(browser, link)
     login_page.open()
     login_page.should_be_login_form()
+
 
 def test_see_form_reg(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
@@ -40,12 +39,11 @@ def test_see_form_reg(browser):
     login_page.open()
     login_page.should_be_register_form()
 
-    
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com"
-    page = ProductPage(browser, link)
+    page = MainPage(browser, link)
     page.open()
     page.should_be_go_to_basket()
     basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_no_goods_in_basket()
     basket_page.should_be_basket_is_empty()
-
